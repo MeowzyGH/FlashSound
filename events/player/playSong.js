3b66898a15dc706eb1f55277464ffc03bd5f3518
MeowzyGH <160866497+MeowzyGH@users.noreply.github.com>
2024-02-22 15:18:56 +0100

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -11,7 +11,7 @@ module.exports = async (client, queue, song) => {
         iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-    .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**`)
+    .setDescription(`\n ‎ \n** **${song?.name}**`)
 .setImage(queue.songs[0].thumbnail)
     .setColor('#FF0000')
     .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
